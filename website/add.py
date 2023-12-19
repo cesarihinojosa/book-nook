331dc910ed307ea224c4de1fe3b83d01a28db53a
Cesar Hinojosa <112355092+CH2080@users.noreply.github.com>
2023-12-18 20:42:46 -0600

    checking for null image; no impl yet

--- website/add.py
+++ website/add.py
@@ -8,10 +8,12 @@ add = Blueprint('add', __name__)
 @add.route('/add', methods=['GET', 'POST'])
 @login_required
 def add_book():
+    books = ''
     if request.method == 'POST':
         query = request.form.get('query')
         print(query)
         books = search(query)
+        assign_values(books)
 
     return render_template("add.html", books=books)
 
@@ -19,5 +21,15 @@ def search(query):
     url = f'https://www.googleapis.com/books/v1/volumes?q={query}'
     response = requests.get(url)
     response_data = response.json()
-    print(response_data["items"][0]["volumeInfo"]["title"])
     return response_data["items"]
+
+def assign_values(data):
+    books = []
+    for i in range(0, 10):
+        book = []
+        if "imageLinks" in data[i]["volumeInfo"]:
+            print("image found")
+        else:
+            print("no image found")
+            
+
